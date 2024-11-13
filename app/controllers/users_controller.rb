f08f173a7bf294838b6cb037e1b4a6e2179fef2b
waykwo <72991933+waykwo@users.noreply.github.com>
2024-11-13 13:59:56 -0500

    Created get user route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def show
+    render json: {
+      id: current_user.id,
+      name: current_user.name,
+      email: current_user.email
+    }
+  end
+  
   def create
     user = User.new(
       name: params[:name],
